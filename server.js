7a4191b45abd5a7e8974d83c0bd8f1760661c7d0
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2024-01-11 09:31:19 +0700

    add cors vao app

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var app = express();
 const routerUser = require('./router/userRouter');
 const routerMessage = require('./router/messageRouter');
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/user/', routerUser);
 app.use('/message', routerMessage); 
 
